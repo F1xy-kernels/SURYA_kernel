ee9d2822b48864165e201a245c3d91beb4064490
Jaegeuk Kim <jaegeuk@google.com>
2020-03-18 12:54:40 -0700

    scsi: ufs: disable hibern8_on_idle
    Let's follow other pixels.
    
    Bug: 151321015
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I5337f0a8a0e75a42cd5d6075d2882cc68f4d0577
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2909,6 +2909,9 @@ static ssize_t ufshcd_hibern8_on_idle_enable_store(struct device *dev,
 	unsigned long flags;
 	u32 value;
 
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
+		return count;
+
 	if (kstrtou32(buf, 0, &value))
 		return -EINVAL;
 
@@ -2946,8 +2949,7 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 	/* initialize the state variable here */
 	hba->hibern8_on_idle.state = HIBERN8_EXITED;
 
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
-	    !ufshcd_is_auto_hibern8_supported(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
 		return;
 
 	if (ufshcd_is_auto_hibern8_supported(hba)) {
@@ -2991,8 +2993,7 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 
 static void ufshcd_exit_hibern8_on_idle(struct ufs_hba *hba)
 {
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
-	    !ufshcd_is_auto_hibern8_supported(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
 		return;
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.delay_attr);
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.enable_attr);
