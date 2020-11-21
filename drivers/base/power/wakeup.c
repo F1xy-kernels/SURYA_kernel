fc8de988d208420bb933f869b1fb104b5c3d19d2
Yaroslav Furman <yaro330@gmail.com>
2020-11-21 20:54:53 +0300

    drivers: remove a few Xiaomi edits
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -939,9 +939,6 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 
 			pr_warn("%s: %d triggered %s\n", __func__,
 					irq_number, name);
-//2020.04.27 add longcheer fengxingqiang "recording wakeup reason"
-			log_wakeup_reason(irq_number);
-
 		}
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
