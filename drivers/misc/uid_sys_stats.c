9ecb0bfb8a6b3faf8a2205712c1a48484517b69c
Yaroslav Furman <yaro330@gmail.com>
2020-11-21 16:06:21 +0300

    Import Xiaomi changes for Poco X3 (NFC)
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/misc/uid_sys_stats.c
+++ drivers/misc/uid_sys_stats.c
@@ -370,7 +370,8 @@ static int uid_cputime_show(struct seq_file *m, void *v)
 		u64 total_stime = uid_entry->stime +
 							uid_entry->active_stime;
 		seq_printf(m, "%d: %llu %llu\n", uid_entry->uid,
-			ktime_to_ms(total_utime), ktime_to_ms(total_stime));
+//2020.04.27 longcheer fengxingqiang edit for "CPU uses statistical time to change to estimate"
+			ktime_to_us(total_utime), ktime_to_us(total_stime));
 	}
 
 	rt_mutex_unlock(&uid_lock);
