df6de3cb50e171c5c4b2e30cf0879ea86645a56b
Yaroslav Furman <yaro330@gmail.com>
2020-12-16 12:37:09 +0300

    nt36xxx: switch to device initcall
    Device boots and dt2w switch works.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
@@ -3345,8 +3345,7 @@ static void __exit nvt_driver_exit(void)
 	spi_unregister_driver(&nvt_spi_driver);
 }
 
-//late_initcall(nvt_driver_init);
-module_init(nvt_driver_init);
+device_initcall(nvt_driver_init);
 module_exit(nvt_driver_exit);
 
 MODULE_DESCRIPTION("Novatek Touchscreen Driver");
