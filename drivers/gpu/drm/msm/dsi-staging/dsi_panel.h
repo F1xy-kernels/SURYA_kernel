d339816befc3a1e916344385b0561788d01fd550
Yaroslav Furman <yaro330@gmail.com>
2020-12-30 04:44:49 +0300

    Revert "Merge remote-tracking branch 'quic/dev/msm-4.14-display' into msm-4.14.c4"
    This reverts commit ecc612b0c0967c5ba24f6305458b0326d9ed270b, reversing
    changes made to 5646253ae744f271a480b4c8c22f21d6589d0c33.
    
    Commit 840d01751b1b94125bd147093b6a6fbae84c9647 causes huge performance
    regressions with DC dimming enabled.
    
    On surya it just fucks everything up.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -92,8 +92,6 @@ struct dsi_dyn_clk_caps {
 	u32 *bit_clk_list;
 	u32 bit_clk_list_len;
 	bool skip_phy_timing_update;
-	enum dsi_dyn_clk_feature_type type;
-	bool maintain_const_fps;
 };
 
 struct dsi_pinctrl_info {
@@ -191,7 +189,6 @@ struct dsi_panel {
 	struct dsi_video_engine_cfg video_config;
 	struct dsi_cmd_engine_cfg cmd_config;
 	enum dsi_op_mode panel_mode;
-	bool panel_mode_switch_enabled;
 
 	struct dsi_dfps_capabilities dfps_caps;
 	struct dsi_dyn_clk_caps dyn_clk_caps;
@@ -199,7 +196,6 @@ struct dsi_panel {
 
 	struct dsi_display_mode *cur_mode;
 	u32 num_timing_nodes;
-	u32 num_display_modes;
 
 	struct dsi_regulator_info power_info;
 	struct dsi_backlight_config bl_config;
@@ -328,14 +324,6 @@ int dsi_panel_send_qsync_off_dcs(struct dsi_panel *panel,
 int dsi_panel_send_roi_dcs(struct dsi_panel *panel, int ctrl_idx,
 		struct dsi_rect *roi);
 
-int dsi_panel_pre_mode_switch_to_video(struct dsi_panel *panel);
-
-int dsi_panel_pre_mode_switch_to_cmd(struct dsi_panel *panel);
-
-int dsi_panel_mode_switch_to_cmd(struct dsi_panel *panel);
-
-int dsi_panel_mode_switch_to_vid(struct dsi_panel *panel);
-
 int dsi_panel_switch(struct dsi_panel *panel);
 
 int dsi_panel_post_switch(struct dsi_panel *panel);
@@ -351,8 +339,5 @@ struct dsi_panel *dsi_panel_ext_bridge_get(struct device *parent,
 int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
-
-void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
-		struct dsi_display_mode *mode, u32 frame_threshold_us);
 
 #endif /* _DSI_PANEL_H_ */
